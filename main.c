26adcbcdad48c1b22ceafbb3235b46761476fce0
flekystyley <flekystyley@gmail.com>
2020-01-31 15:04:18 +0900

    update main.c;

--- main.c
+++ main.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <getopt.h>
 
-void print_usage()
-{
-    printf("Usage: slowC option args\n\n");
-    printf(
-    "Available options:\n"
-    " c - contentLength.\n"
-    " d - dialWorkerCount.\n"
-    " g - goMaxProcs.\n"
-    " r - rampUpInterval.\n"
-    " s - sleepInterval.\n"
-    " t - testDuration.\n"
-    " v - victimURL.\n"
-    " h - hostHeader.\n"
-    );
-    return;
-}
+#include "args_parser.h"
 
 int main(int argc, char *argv[])
-{   
-    if (argc < 2)
-    {
-        print_usage();
-        return 2;
+{
+    Arguments args;
+    init_args(&args);
+    if (args.help) {
+        print_help(argv[0]);
+        exit(0);
     }
-
     return 0;
 }
