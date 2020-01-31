f7c9ec19fa87b9e2e3c5636fa299399e163b38be
flekystyley <flekystyley@gmail.com>
2020-02-01 01:07:44 +0900

    update main.c;

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "args_parser.h"
 
+#define UNUSED(expr) do { (void)(expr); } while (0)
+
 int main(int argc, char const *argv[])
 {
+    UNUSED(argc);
     Arguments args;
     init_args(&args);
     if (args.help) {
