dace52316af16e9d0d2bc448eefd8a258434e436
flekystyley <flekystyley@gmail.com>
2020-02-22 15:36:56 +0900

    fix args_parser.c;

--- src/args_parser.c
+++ src/args_parser.c
@@ -18,8 +18,8 @@ void init_args(Arguments *args)
     args->ramp_up_interval = 10;
     args->sleep_interval = 10*10;
     args->test_duration = 0;
-    args->victim_url = "127.0.0.1";
-    args->host_header = NULL;
+    args->target_host = "127.0.0.1";
+    args->target_port = 80;
 }
 
 void print_usage(const char *program)
@@ -33,20 +33,20 @@ void print_help(const char *program)
     printf(
     "Available options:\n"
     " -H - help command\n"
-    " -c - contentLength.\n"
-    " -d - dialWorkerCount.\n"
-    " -r - rampUpInterval.\n"
-    " -s - sleepInterval.\n"
-    " -t - testDuration.\n"
-    " -v - victimURL.\n"
-    " -h - hostHeader.\n"
+    " -c - content length.\n"
+    " -d - dial worker count.\n"
+    " -r - rampup interval.\n"
+    " -s - sleep interval.\n"
+    " -t - test duration.\n"
+    " -h - target host.\n"
+    " -p - target port.\n"
     );
 }
 
 void parse(int argc, char const *argv[], Arguments *args)
 {
     int opt;
-    while ((opt = getopt(argc, (char *const*)argv, "Hc:d:r:s:t:v:h:")) != -1) {
+    while ((opt = getopt(argc, (char *const*)argv, "Hc:d:r:s:t:h:p:")) != -1) {
         switch (opt) {
         case 'H':
             print_help(argv[0]);
@@ -66,11 +66,10 @@ void parse(int argc, char const *argv[], Arguments *args)
         case 't':
             args->test_duration = atoi(optarg);
             break;
-        case 'v':
-            args->victim_url = optarg;
         case 'h':
-            args->host_header = optarg;
-            break;
+            args->target_host = optarg;
+        case 'p':
+            args->target_port = atoi(optarg);
         default:
             printf("ERROR: Unknown arguments passed\n");
             print_usage(argv[0]);
